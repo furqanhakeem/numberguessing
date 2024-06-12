89f3e220dc61fa012f9d34aa8136263aa3f8191e
furqanhakeem <furqanhakim_85@hotmail.com>
2024-06-12 20:57:19 +0500

    Simple Number Guessing

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node    
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const guessnumber = await inquirer.prompt([
+    {
+        name: "usreguessnumber",
+        type: "number",
+        message: "Please Guess Your Number between 1 to 10",
+    },
+]);
+if (guessnumber.usreguessnumber === randomnumber) {
+    console.log("Your Guess is Correct :) :) :)");
+}
+else
+    console.log("You Have Guessed it Wrong :( :( :(");
